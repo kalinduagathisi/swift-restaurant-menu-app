821b17a6c7e8ecc8762d9b23ff9d7501d2955c92
kalinduagathisi <kalinduagathisi@gmail.com>
2024-11-08 12:52:18 +0530

    Initial Commit

---
+++ test app swiftui/test_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_swiftuiApp.swift
+//  test app swiftui
+//
+//  Created by Kalindu Agathisi on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct test_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test app swiftuiTests/test_app_swiftuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_swiftuiTests.swift
+//  test app swiftuiTests
+//
+//  Created by Kalindu Agathisi on 2024-11-08.
+//
+
+import Testing
+@testable import test_app_swiftui
+
+struct test_app_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
